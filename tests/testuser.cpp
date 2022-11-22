7bc62f1335a990b7c9fddbab85507599790b26ce
Buschmann <mf@huessenbergnetz.de>
2022-11-22 11:36:48 +0100

    [common] add some tests for User object

---
+++ tests/testuser.cpp
@@ -0,0 +1,86 @@
+/*
+ * SPDX-FileCopyrightText: (C) 2022 Matthias Fehring <https://www.huessenbergnetz.de>
+ * SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include "user.h"
+
+#include <QTest>
+#include <QDataStream>
+
+class UserTest : public QObject
+{
+    Q_OBJECT
+public:
+    UserTest(QObject *parent = nullptr) : QObject{parent} {
+
+    }
+
+    ~UserTest() final = default;
+
+private slots:
+    void initTestCase() {}
+
+    void testDefaultConstructor();
+    void testConstructorWithArgs();
+    void testMove();
+
+    void cleanupTestCase() {}
+};
+
+void UserTest::testDefaultConstructor()
+{
+    User user;
+    QVERIFY(user.isNull());
+    QVERIFY(!user.isValid());
+}
+
+void UserTest::testConstructorWithArgs()
+{
+    const QDateTime now = QDateTime::currentDateTimeUtc();
+    const QVariantMap settings = QVariantMap({
+                                                 {QStringLiteral("option"), QStringLiteral("value")}
+                                             });
+    User user(1, User::Registered, QStringLiteral("user"), QStringLiteral("user@example.net"), now, now, QDateTime(), now, QDateTime(), 0, settings);
+
+    QCOMPARE(user.id(), 1);
+    QCOMPARE(user.type(), User::Registered);
+    QCOMPARE(user.username(), QStringLiteral("user"));
+    QCOMPARE(user.email(), QStringLiteral("user@example.net"));
+    QCOMPARE(user.created(), now);
+    QCOMPARE(user.updated(), now);
+    QCOMPARE(user.validUntil(), QDateTime());
+    QCOMPARE(user.lastSeen(), now);
+    QCOMPARE(user.lockedAt(), QDateTime());
+    QCOMPARE(user.lockedBy(), 0);
+    QCOMPARE(user.settings(), settings);
+}
+
+void UserTest::testMove()
+{
+    const QDateTime now = QDateTime::currentDateTimeUtc();
+
+    // test move constructor
+    {
+        User user1(1, User::Registered, QStringLiteral("user"), QStringLiteral("user@example.net"), now, now, QDateTime(), now, QDateTime(), 0, QVariantMap());
+        User user2(std::move(user1));
+        QCOMPARE(user2.id(), 1);
+        QCOMPARE(user2.username(), QStringLiteral("user"));
+        QCOMPARE(user2.email(), QStringLiteral("user@example.net"));
+        QCOMPARE(user2.created(), now);
+    }
+
+    // test move assignment
+    {
+        User u1(1, User::Registered, QStringLiteral("user1"), QStringLiteral("user1@example.net"), now, now, QDateTime(), now, QDateTime(), 0, QVariantMap());
+        User u2(2, User::Registered, QStringLiteral("user2"), QStringLiteral("user2@example.net"), now, now, QDateTime(), now, QDateTime(), 0, QVariantMap());
+        u2 = std::move(u1);
+        QCOMPARE(u2.id(), 1);
+        QCOMPARE(u2.username(), QStringLiteral("user1"));
+        QCOMPARE(u2.email(), QStringLiteral("user1@example.net"));
+    }
+}
+
+QTEST_MAIN(UserTest)
+
+#include "testuser.moc"
