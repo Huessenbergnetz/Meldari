05e3928b475c4011dac658880dac7d15f38285d1
Buschmann <mf@huessenbergnetz.de>
2022-12-02 12:57:16 +0100

    [app] add translation files

---
+++ app/translations/meldari.de.ts
@@ -0,0 +1,144 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1" language="de_DE" sourcelanguage="en">
+<context>
+    <name>ControlCenter</name>
+    <message>
+        <source>Dashboard</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Arrrgh, bad username and/or password!</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <comment>site title</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Username</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Password</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Login</source>
+        <comment>button text</comment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Logout</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenterUsers</name>
+    <message>
+        <source>This path only accepts POST requests.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>You do not have the required permissions.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Users</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ControlCenterUsersettings</name>
+    <message>
+        <source>My settings</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Error</name>
+    <message>
+        <source>Bad request</source>
+        <extracomment>Error title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Access denied</source>
+        <extracomment>Error title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Not found</source>
+        <extracomment>Error title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Method not allowed</source>
+        <extracomment>Error title</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Internal server error</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>Root</name>
+    <message>
+        <source>Access denied</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>CSRF check failed.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>SimpleUser</name>
+    <message>
+        <source>Failed to query list of users from the database.</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>User</name>
+    <message>
+        <source>Failed to query list of users from the database.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Invalid</source>
+        <extracomment>user type</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Disabled</source>
+        <extracomment>user type</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Registered</source>
+        <extracomment>user type</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Administrator</source>
+        <extracomment>user type</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <source>Super User</source>
+        <extracomment>user type</extracomment>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
