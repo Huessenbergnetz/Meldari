2c043ac33c43b2487873bd14f05e0e63a7f411de
Buschmann <mf@huessenbergnetz.de>
2022-12-04 12:52:37 +0100

    [app] UserAuthStoreSql: cleanup

--- app/userauthstoresql.cpp
+++ app/userauthstoresql.cpp
@@ -33,7 +33,7 @@ AuthenticationUser UserAuthStoreSql::findUser(Context *c, const ParamsMultiMap &
 
     const QString username = userinfo.value(QStringLiteral("username"));
 
-    QSqlQuery q = CPreparedSqlQueryThreadFO(QStringLiteral("SELECT id, type, password, email, created_at, updated_at, valid_until, last_seen, locked_at, locked_by FROM users WHERE username = :username"));
+    QSqlQuery q = CPreparedSqlQueryThreadFO(QStringLiteral("SELECT id, type, password FROM users WHERE username = :username"));
     q.bindValue(QStringLiteral(":username"), username);
 
     if (Q_LIKELY(q.exec())) {
@@ -42,26 +42,6 @@ AuthenticationUser UserAuthStoreSql::findUser(Context *c, const ParamsMultiMap &
             user.insert(QStringLiteral("username"), username);
             user.insert(QStringLiteral("type"), q.value(1));
             user.insert(QStringLiteral("password"), q.value(2));
-            user.insert(QStringLiteral("email"), q.value(3));
-            user.insert(QStringLiteral("created_at"), q.value(4));
-            user.insert(QStringLiteral("updated_at"), q.value(5));
-            user.insert(QStringLiteral("valid_until"), q.value(6));
-            user.insert(QStringLiteral("last_seen"), QDateTime::currentDateTimeUtc());
-            user.insert(QStringLiteral("locked_at"), q.value(8));
-            user.insert(QStringLiteral("locked_by"), q.value(9));
-
-            QSqlQuery qq = CPreparedSqlQueryThreadFO(QStringLiteral("SELECT name, value FROM usersettings WHERE user_id = :user_id"));
-            qq.bindValue(QStringLiteral(":user_id"), user.id());
-
-            if (Q_LIKELY(qq.exec())) {
-                QVariantMap settings;
-                while (qq.next()) {
-                    settings.insert(qq.value(0).toString(), qq.value(1));
-                }
-                user.insert(QStringLiteral("settings"), settings);
-            } else {
-                qCCritical(MEL_AUTHN) << "Failed to execute database query to get usersettings for user" << username << "from the database:" << qq.lastError().text();
-            }
 
         } else {
             qCWarning(MEL_AUTHN, "Login failed: username %s not found. IP: %s", qUtf8Printable(username), qUtf8Printable(c->req()->addressString()));
