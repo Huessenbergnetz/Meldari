be309c79a34609d1a1fbff93b3ba3bd81bd05d8b
Buschmann <mf@huessenbergnetz.de>
2022-12-08 11:47:44 +0100

    [commonjs] general.js: add functions to Date to add days, weeks, etc.

--- commonjs/general.js
+++ commonjs/general.js
@@ -3,6 +3,29 @@
  * SPDX-License-Identifier: AGPL-3.0-or-later
  */
 
+Date.prototype.addDays = function(days) {
+    let date = new Date(this.valueOf());
+    date.setDate(date.getDate() + days);
+    return date;
+}
+
+Date.prototype.addWeeks = function(weeks) {
+    let date = new Date(this.valueOf());
+    return date.addDays(weeks * 7);
+}
+
+Date.prototype.addMonths = function(months) {
+    let date = new Date(this.valueOf());
+    date.setMonth(date.getMonth() + months);
+    return date;
+}
+
+Date.prototype.addYears = function(years) {
+    let date = new Date(this.valueOf());
+    date.setFullYear(date.getFullYear() + years);
+    return date;
+}
+
 var Meldari = Meldari || {};
 
 Meldari.newXhrHeaders = function() {
