412dead8d5834ec96dbeaffb5ce7d479acaede16
Buschmann <mf@huessenbergnetz.de>
2022-12-08 12:25:11 +0100

    [template] remove obsolete MeldariTmpl.createInputDate() method
    Use Date.prototype.toInputForm() instead

--- templates/meldari/assets/js/general.js
+++ templates/meldari/assets/js/general.js
@@ -17,21 +17,6 @@ MeldariTmpl.newXhrHeaders = function() {
     return new Headers({'X-Requested-With': 'XMLHttpRequest'});
 }
 
-MeldariTmpl.createInputDate = function(date) {
-    if (date.constructor.name !== 'Date') {
-        console.error('createInputDate expects a Date object as input value!');
-        return '';
-    }
-
-    const y = date.getFullYear();
-    const m = date.getMonth() + 1;
-    const d = date.getDate();
-    const hrs = date.getHours();
-    const min = date.getMinutes();
-    const rv = y + '-' + (m < 10 ? '0' : '') + m + '-' + (d < 10 ? '0' : '') + d + 'T' + (hrs < 10 ? '0' : '') + hrs + ':' + (min < 10 ? '0' : '') + min;
-    return rv;
-}
-
 MeldariTmpl.switchButton = function(button) {
     if (button.disabled) {
         button.disabled = false;
